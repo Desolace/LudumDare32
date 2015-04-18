711b54333165d5e7ea8587b7694edc66739c54fb
Kevin Dean <kevinedean@gmail.com>
2015-04-18 15:53:47 -0500

    levels can specify real actors

--- level.py
+++ level.py
@@ -1,29 +1,38 @@
 import json
 import pygame
+from actor import Actor
 
 class Level(object):
-    _levelDef = None
-    surface = None
-    _tileSize = None
 
-    def __init__(self, levelName):
-        with open(levelName, "r") as levelData:
-            self._levelDef = json.load(levelData)
-            self.width = self._levelDef['w']
-            self.height = self._levelDef['h']
+    def __init__(self, level_name, physics_manager):
+        with open(level_name, "r") as levelData:
+            self._level_def = json.load(levelData)
+            self.width = self._level_def['w']
+            self.height = self._level_def['h']
+            self.actors = []
+            self._tile_size = None
+
+            physics_manager.world_width = self.width
+            physics_manager.world_height = self.height
 
             self.surface = pygame.Surface((self.width, self.height))
-            pixels = pygame.PixelArray(self.surface)
 
-            for item in self._levelDef['map']:
-                x = item['x']
-                y = item['y']
-                color = tuple(self._levelDef['materials'][item['mat']])
+            for item in self._level_def['map']:
+                material = self._level_def['materials'][item['mat']]
+                color = tuple(material['color'])
+                subsurface = pygame.Surface((item['w'], item['h']))
                 for dx in range(0, item['w']):
                     for dy in range(0, item['h']):
-                        pixels[x+dx, y+dy] = color
+                        subsurface.set_at((dx, dy), color)
+                if item.get("bg"): #its a background item, not interactable with the world
+                    self.surface.blit(subsurface, (item['x'], item['y']))
+                else: #its an actor in the world
+                    new_actor = Actor(subsurface, item['w'], item['h'])
+                    physics_manager.add_actor(new_actor, weight=material["weight"])
+                    physics_manager.set_position(new_actor, (item['x'], item['y']))
+                    self.actors.append(new_actor)
 
-    def update(self, tileSize):
-        if(tileSize != self._tileSize):
-            self.surface = pygame.transform.scale(self.surface, (self.width * tileSize, self.height * tileSize))
-            self._tileSize = tileSize
+    def update(self, tile_size):
+        if(tile_size != self._tile_size):
+            self.surface = pygame.transform.scale(self.surface, (self.width * tile_size, self.height * tile_size))
+            self._tile_size = tile_size
